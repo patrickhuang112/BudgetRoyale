837ff00b6bb712c623f270ab2d9b70e23b5e2dc7
walid101 <nuhankhan101@gmail.com>
2021-01-07 12:01:45 -0500

    Added 5 models types, to models.py. Each model can be used as a struct in database.

--- budgetroyale/models.py
+++ budgetroyale/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class BudgetSubmission(models.Model):
+    title = models.CharField(max_length=255);
+    budgetPlan = models.TextField();
+    BudgetCateg = models.CharField(max_length=255);
+    yesVotes = models.IntegerField();
+
+class User(models.Model):
+    score = models.IntegerField();
+    name = models.CharField(max_length = 255);
+    ID = models.CharField(max_length = 12);
+    rank = models.IntegerField();
+
+class Room(models.Model):
+    roomCode = models.charField(max_length = 20);
+
+    user1 = User()
+    budgetSub1 = BudgetSubmission();
+
+    user2 = User()
+    budgetSub1 = BudgetSubmission();
+
+    user3 = User()
+    budgetSub1 = BudgetSubmission();
+
+    user4 = User()
+    budgetSub1 = BudgetSubmission();
+
+    user5 = User();
+    budgetSub1 = BudgetSubmission();
+
--- budgetroyale/views.py
+++ budgetroyale/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def room(request, *args, **kwargs):
+    if request.GET.get('create'):
+        print('user clicked create')
+
+    return render(request, 'room.html');
