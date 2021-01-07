659760da28acccc557b2fb200ffb646a19d73999
walid101 <nuhankhan101@gmail.com>
2021-01-07 12:17:48 -0500

    added back all views, based on html in templates

--- budgetroyale/views.py
+++ budgetroyale/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def judge(request):
+    return render(request, 'judge.html')
+
+def room(request):
+    return render(request, 'room.html')
+
+def submit(request):
+    return render(request, 'submit.html')
