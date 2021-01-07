f93479c0ac3b94313916bb6e262f398177157f72
axk5209 <arhamkothari@utexas.edu>
2021-01-07 13:36:24 -0600

    cleaner model

--- budgetroyale/models.py
+++ budgetroyale/models.py
@@ -1,30 +1,25 @@
 from django.db import models
 
-class BudgetSubmission(models.Model):
-    title = models.CharField(max_length=255, default="");
-    budgetPlan = models.TextField(default="");
-    BudgetCateg = models.CharField(max_length=255, default = "");
-    yesVotes = models.IntegerField(default=0);
-
-class User(models.Model):
-    score = models.IntegerField(default=0);
-    name = models.CharField(max_length = 255, default = "");
-    rank = models.IntegerField(default=0);
 
 class Room(models.Model):
-    roomCode = models.CharField(max_length = 20, default = "");
+    roomCode = models.CharField(max_length=20, default="")
+    contestantsCount = models.IntegerField(default=0)
+    # JSON string representing list of budget submissions
+    submissions = models.TextField(max_length=10000, default="")
+    # 0 - not started, #1 - waiting for submissions, #2 - waiting for reviews
+    state = models.IntegerField(default=0)
 
-    user1 = User()
-    budgetSub1 = BudgetSubmission();
 
-    user2 = User()
-    budgetSub1 = BudgetSubmission();
+class User(models.Model):
+    score = models.IntegerField(default=0)
+    name = models.CharField(max_length=255, default="")
+    rank = models.IntegerField(default=0)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
-    user3 = User()
-    budgetSub1 = BudgetSubmission();
 
-    user4 = User()
-    budgetSub1 = BudgetSubmission();
-
-    user5 = User();
-    budgetSub1 = BudgetSubmission();
+class BudgetSubmission(models.Model):
+    title = models.CharField(max_length=255, default="")
+    budgetPlan = models.TextField(default="")
+    BudgetCateg = models.CharField(max_length=255, default="")
+    yesVotes = models.IntegerField(default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
