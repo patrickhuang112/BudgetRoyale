46a7cfe1bfff8dbc540a3d9118e99e45051cbfe3
Patrick Huang <pat.b.huang@gmail.com>
2021-01-06 22:48:57 -0800

    updated a bunch of templates, began working on the flip page functionality in judge, and set up basic input checking in submit

--- budgetroyale/views.py
+++ budgetroyale/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def submit(request):
+    return render(request, 'submit.html')
+
+def judge(request):
+    return render(request, 'judge.html')
