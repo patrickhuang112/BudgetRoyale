3153faef37fc15b35b1dfff60428f852bf48f311
walid101 <nuhankhan101@gmail.com>
2021-01-08 01:12:19 -0500

    changed things in settings

--- mysite1/settings.py
+++ mysite1/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite1',
+    'budgetroyale',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +122,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
@@ -129,4 +132,3 @@ STATICFILES_DIRS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
